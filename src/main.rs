4d9eaea1dd70d81ceaf2631dad4621693d9ad551
Alexander Galibey <alexander.galibey@infinyon.com>
2023-01-24 12:47:13 -0500

    feat: migrate mqtt-source connector to new SDK

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-pub fn main() {
-    println!("Hello world!");
+mod config;
+mod error;
+mod event;
+mod formatter;
+mod source;
+
+use config::MqttConfig;
+
+use fluvio::{RecordKey, TopicProducer};
+use fluvio_connector_common::{
+    connector,
+    tracing::{debug, trace},
+    Result, Source,
+};
+use futures::StreamExt;
+use source::MqttSource;
+
+#[connector(source)]
+async fn start(config: MqttConfig, producer: TopicProducer) -> Result<()> {
+    debug!(?config);
+    let source = MqttSource::new(&config)?;
+    let mut stream = source.connect(None).await?;
+    while let Some(item) = stream.next().await {
+        trace!(?item);
+        producer.send(RecordKey::NULL, item).await?;
+    }
+    Ok(())
 }
